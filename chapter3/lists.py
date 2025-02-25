097702d60256ebc3b977bb59194cf859aac0b836
PhomelloP <112708560+PhomelloP@users.noreply.github.com>
2025-02-26 01:14:27 +0200

    Add files via upload

---
+++ chapter3/lists.py
@@ -0,0 +1,23 @@
+Alist=["Aidan","Asanda","Cadee","Courtney","Ethan","Lindo","Mieke","Phomello","Ronny","Sibu","Tom","Ulrich"]
+
+print(Alist)
+
+print(Alist.pop())
+
+Alist.insert(3,"Sihle")
+print(Alist)
+
+Alist.append("Thato")
+print(Alist)
+
+print(Alist[7])
+
+print(Alist[::2])
+
+print(Alist[3])
+
+Blist=f'This is the top 3 for Term 2{Alist[3:6]} Congratulations'
+print(Blist)
+
+Alist.sort()
+print(Alist)
---
+++ chapter3/try_it_yourself/seeing_the_world.py
@@ -0,0 +1,56 @@
+#Friends 
+#friends=["Zinhle","Uyanda","Boitumelo","Thandiswa"]
+
+#for x in friends: 
+ # print(x)
+
+#for x in friends:
+ #  print (f"Hey {x}, Lets make the best of this week.")
+
+#Dinner guest
+
+#dinnerguests=["Lihle","Queen","Ofentse"]
+
+#for guest in dinnerguests:
+ #  print(f"My dearest {guest}, Kindly join me for a fun evening filled with tasty treats.")
+
+#print(dinnerguests[2])
+
+#dinnerguests[2]="Mbali"
+#print(dinnerguests)
+
+#for guest in dinnerguests:
+ #  print(f"My dearest {guest}, Kindly join me for a fun evening filled with tasty treats, joining us will also be{dinnerguests}.")
+
+#dinnerguests.insert(0,"Zanele")
+#print(dinnerguests)
+
+#dinnerguests.insert(2,"Kwanele")
+#print(dinnerguests)
+
+#dinnerguests.append("Mohau")
+#print(dinnerguests)
+
+#for guest in dinnerguests:
+ #  print(f"Dear {guest}, in light of recent events our guest list has been expanded, joining us for dinner will now be{dinnerguests}")
+
+#dinnerguests.pop(0)
+#for guest in dinnerguests:
+ #  print(f"So sorry {guest} I have to change the lsit again and cancel a few of you , now only two will be joining me.")
+
+
+Locations=["Bali","Timbuktu","Peru","Morocco","Tanzania"]
+
+print(Locations)
+
+print(sorted(Locations))
+
+print(Locations)
+
+Locations.sort(reverse=True)
+
+print(Locations)
+
+Locations.sort()
+
+print(Locations)
