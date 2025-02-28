85b2b922f01e7f3589e2861627505795678923c0
PhomelloP <112708560+PhomelloP@users.noreply.github.com>
2025-02-28 02:11:32 +0200

    Add files via upload

---
+++ chapter7/input.py
@@ -0,0 +1,9 @@
+# rental_car=input("what kind of car are you looking for? ")
+# print(f'Let me see if I can find a {rental_car} for you.')
+
+# num_of_guests= int(input("How many Seats do you need? "))
+# if num_of_guests >= 8:
+#     print("Sorry we are at capacity at the moment, feel free to wait.")
+# else:
+#     print("Your table is ready!")
+
