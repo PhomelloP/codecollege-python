60c8f0abe0b0e5dba3877552de8ea15c2399216b
PhomelloP <112708560+PhomelloP@users.noreply.github.com>
2025-02-28 02:10:29 +0200

    Add files via upload

---
+++ chapter5/If_else.py
@@ -0,0 +1,89 @@
+# # #cars
+# cars=["kia","datsun","opel","Toyota","honda"]
+
+# for car in cars:
+#     if car == "kia":
+#         print(car.upper())
+#     else:
+#         print(car.title())
+        
+#pizza
+# requestedTopping="Pineapple"
+# if requestedTopping != "Jalepeno":
+#     print("That is not what I ordered")
+
+
+#age
+# age=0
+
+# if age >= 18:
+#     print("Enjoy the Open bar")
+# elif age <= 0:
+#     print("Seems like you have not been born yet")
+# else:
+#     print("Sorry, Underage drinking is illegal.")
+#     print("We've got sodas and juice boxes")  
+
+age_0=24
+age_1=15
+
+if age_0 >= 18 and age_1 >=18:
+    print("Enjoy the Open bar")
+elif age_0<= 18 or age_1 <= 18:
+    print("oh no!Only one can drink ")
+
+
+#Aliens
+
+# alien="blue"
+
+# if alien == "green":
+#     print("You are 5 points up!")
+# elif alien == "yellow":
+#     print("You are 10 points up!")
+# elif alien =="red":
+#     print("You are 15 points up!")
+# else:
+#     print("hmm we don't recognize that colour")
+        #will continue following video after chapter 6, follow all examples pls
+
+# #weather
+
+# weather="sunny"
+# if weather == "sunny":
+#     print("Its really Sunny today")
+# else:
+#     print("I long for Sunny days")
+
+# #Grades
+# grade=75
+
+# if grade >= 75:
+#     print("")
+ 
+#  #Moods
+
+# mood = ['happy', 'sad', 'angry', 'excited', 'tired', 'bored', 'hungry', 'confused', 'anxious', 'stressed']
+# for mood in mood:
+#     if 'happy' in mood:
+#         print("I am happy!")
+#     elif 'sad' in mood:
+#         print("I am sad.")
+#     elif 'angry' in mood:
+#         print("I am angry.")
+#     elif 'excited' in mood:
+#         print("I am excited!")
+#     elif 'tired' in mood:
+#         print("I am tired.")
+#     elif 'bored' in mood:
+#         print("I am bored.")
+#     elif 'hungry' in mood:
+#         print("I am hungry.")
+#     elif 'confused' in mood:
+#         print("I am confused.")
+#     elif 'anxious' in mood:
+#         print("I am anxious.")
+#     elif 'stressed' in mood:
+#         print("I am stressed.")
+#     else:
+#         print("I am feeling fine.")
